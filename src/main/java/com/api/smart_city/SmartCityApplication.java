3344aff662e99cf71fea820fd0f73fdf9a89fa32
Sophanorin <heousopharin168@gmail.com>
2021-10-26 21:59:56 +0700

    enable cors

--- src/main/java/com/api/smart_city/SmartCityApplication.java
+++ src/main/java/com/api/smart_city/SmartCityApplication.java
@@ -2,8 +2,10 @@ package com.api.smart_city;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin(origins = "*")
 public class SmartCityApplication {
 
     public static void main(String[] args) {
